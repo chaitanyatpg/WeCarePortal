a516d4254962693c74f7c4e418bb970c9826c5a9
Dhruv Ranjan <dhruv.ranjan@gmail.com>
2020-06-09 20:31:26 -0700

    Debug off

--- mycareportal/settings.py
+++ mycareportal/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'ylwi3hn#79&v2(%#k&por1szkfssgih4nn)13t4q9v0sz47*eg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.1.64','mycareportal.herokuapp.com','localhost','www.wecareportal.com','www.wecarehealthportal.com','127.0.0.1']
 #'192.168.2.77'
