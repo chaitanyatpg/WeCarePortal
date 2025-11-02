07db7485baea70b3fd92333a035c3cf9383f350a
uekchaitanya <uekchaitanya@gmail.com>
2025-11-02 17:26:10 -0500

    Fix static files 500 error - use simpler whitenoise storage
    CompressedManifestStaticFilesStorage requires proper collectstatic manifest
    Use CompressedStaticFilesStorage instead to avoid manifest issues

--- mycareportal/settings.py
+++ mycareportal/settings.py
@@ -212,8 +212,8 @@ LOCALE_PATHS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Whitenoise settings for serving static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Whitenoise settings for serving static files - use simple storage to avoid manifest issues
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "mycareportal_app/static")
