1eeafcaf1ca02da3bc93af31a6df18a574313511
uekchaitanya <uekchaitanya@gmail.com>
2025-11-02 20:15:20 -0500

    Pin whitenoise to 5.3.0 for CompressedManifestStaticFilesStorage compatibility
    - Update whitenoise from 3.3.0 to 5.3.0 in requirements.txt
    - Restore CompressedManifestStaticFilesStorage setting for proper static file compression
    - Fixes ImportError with whitenoise storage classes

--- mycareportal/settings.py
+++ mycareportal/settings.py
@@ -205,7 +205,7 @@ LOCALE_PATHS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "mycareportal_app/static")
