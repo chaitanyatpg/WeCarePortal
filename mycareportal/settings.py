6aedd70f070ed28b9d1b5651c0d3c6deceb46b7d
uekchaitanya <uekchaitanya@gmail.com>
2025-11-02 20:20:44 -0500

    Temporarily use basic StaticFilesStorage to resolve manifest issues
    - Switch from CompressedManifestStaticFilesStorage to basic StaticFilesStorage
    - Allows site to boot while collectstatic/manifest issues are resolved
    - Static files will still be served by whitenoise middleware

--- mycareportal/settings.py
+++ mycareportal/settings.py
@@ -205,7 +205,7 @@ LOCALE_PATHS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "mycareportal_app/static")
